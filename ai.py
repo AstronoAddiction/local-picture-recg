ed379982e9fca4e35aa60035ac23df9755802e4b
AstronoAddiction <64092448+AstronoAddiction@users.noreply.github.com>
2025-07-14 10:54:19 +0800

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,25 @@
+import dashscope
+from dashscope import MultiModalConversation
+import json
+
+dashscope.api_key = 'sk-47407d15aec84552bbc717d5b8a83886'
+
+image_path = 'mountain.jpg'
+
+question = '这张图片里有什么？'
+
+response = MultiModalConversation.call(
+    model='qwen-vl-plus',
+    messages=[
+        {
+            'role': 'user',
+            'content': [
+                {'image': image_path},
+                {'text': question}
+            ]
+        }
+    ],
+    result_format='message'
+)
+
+print(response['output']['choices'][0]['message']['content'][0]['text'])
